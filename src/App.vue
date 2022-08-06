ef22162fc415ea80ad8c09ecf98f6385af5f7724
anerjun <917464064@qq.com>
2022-08-07 00:05:53 +0800

    新增了axios组件及编写了基地址

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
     <van-button type="default">默认按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
+    <div class="box">我是盒子</div>
   </div>
 </template>
 
@@ -21,4 +22,9 @@ export default {
 </script>
 
 <style lang="less">
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: pink;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
 import './styles/index.less'
+// 加载Vant 自动按需导入
 import { Button } from 'vant'
+// 动态设置rem基准值
+import 'amfe-flexible'
 Vue.use(Button)
 
 Vue.config.productionTip = false
