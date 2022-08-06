7774466d56254efaafa3062bf13f8fda1c38e442
anerjun <917464064@qq.com>
2022-08-06 17:21:28 +0800

    我修改了文件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
     黑马头条
     <!-- 路由出口 -->
     <router-view />
+    <div>
+      <i class="iconfont icon-shouji"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
